3fc8ea3ea7192e60c8825d1139d08e94bc2bb472
hm5829 <hm5829@yahoo.com.tw>
2017-11-12 14:43:29 +0800

    change

--- app/src/main/java/com/tom/practice/MainActivity.java
+++ app/src/main/java/com/tom/practice/MainActivity.java
@@ -4,19 +4,33 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button bHelp = findViewById(R.id.b_help);
+        bHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage("BMI等於體重除以身高平方")
+                        .setTitle("BMI說明")
+                        .setPositiveButton("ok",null)
+                        .show();
+            }
+        });
+        };
+
+
+
     public void bmi(View v){
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight = findViewById(R.id.ed_height);
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);
         float w = Float.parseFloat(edWeight.getText().toString());
         float h = Float.parseFloat(edHeight.getText().toString());
         float bmi = w / (h*h);
